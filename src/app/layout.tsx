4f3cbb7e95f2c6a25a37cb85fe8029eb11e786eb
Sandip Gyawali <sandipgyawali10@gmail.com>
2025-08-18 22:09:10 +0545

    feat(add): tailwind config add, theme switch ingration and theme-provider with next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/Layouts/Default";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,16 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
-        <Layout>{children}</Layout>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Layout>{children}</Layout>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/Switch/ThemeSwitcher.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useId } from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { Switch } from "./Switch";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const id = useId();
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Fix hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const isDark = resolvedTheme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(isDark ? "light" : "dark");
+  };
+
+  return (
+    <div
+      className="group inline-flex items-center gap-2"
+      data-state={isDark ? "checked" : "unchecked"}
+    >
+      <span
+        id={`${id}-off`}
+        className="group-data-[state=checked]:text-muted-foreground/70 flex-1 cursor-pointer text-right text-sm font-medium"
+        aria-controls={id}
+        onClick={() => setTheme("dark")}
+      >
+        <MoonIcon size={16} aria-hidden="true" />
+      </span>
+      <Switch
+        id={id}
+        checked={!isDark}
+        onCheckedChange={toggleTheme}
+        aria-labelledby={`${id}-off ${id}-on`}
+        aria-label="Toggle between dark and light mode"
+      />
+      <span
+        id={`${id}-on`}
+        className="group-data-[state=unchecked]:text-muted-foreground/70 flex-1 cursor-pointer text-left text-sm font-medium"
+        aria-controls={id}
+        onClick={() => setTheme("light")}
+      >
+        <SunIcon size={16} aria-hidden="true" />
+      </span>
+    </div>
+  );
+}
