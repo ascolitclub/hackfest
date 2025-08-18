93744d9bb93f56ee350684ed25af5c0b371de1aa
Sandip Gyawali <sandipgyawali10@gmail.com>
2025-08-18 22:05:49 +0545

    feat(add): doodles svg image integration for landing page

---
+++ src/components/Doodles/Line.tsx
@@ -0,0 +1,43 @@
+export const Line = ({ className = "", color = "currentColor", size = 24 }) => {
+  return (
+    <svg
+      color="#F4F2F8"
+      viewBox="0 0 92 47"
+      fill={color}
+      width={size}
+      height={size}
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {" "}
+      <path
+        d="M2 45.0999L35.2 22.3999"
+        stroke={color}
+        strokeWidth="5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />{" "}
+      <path
+        d="M2 45.0999L35.2 22.3999"
+        stroke={color}
+        strokeWidth="5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />{" "}
+      <path
+        d="M35.2002 40.1L89.6002 2"
+        stroke={color}
+        strokeWidth="5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />{" "}
+      <path
+        d="M35.2002 40.1L89.6002 2"
+        stroke={color}
+        strokeWidth="5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
